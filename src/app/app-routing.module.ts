80bcb0ff35a20861f944447d2c164d512b038853
Guddu Kumar <coldmilkhottea@gmail.com>
2024-07-27 01:20:05 +0530

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/pages/home/home.component';
+import { BlogComponent } from './component/pages/blog/blog.component';
+import { ContactUsComponent } from './component/pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: HomeComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/global/header/header.component';
+import { FooterComponent } from './component/global/footer/footer.component';
+import { HomeComponent } from './component/pages/home/home.component';
+import { BlogComponent } from './component/pages/blog/blog.component';
+import { ContactUsComponent } from './component/pages/contact-us/contact-us.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,

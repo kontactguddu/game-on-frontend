ffda30bfb9d3eb69b63ea4ed8c3f3d30dd952d7d
Guddu Kumar <coldmilkhottea@gmail.com>
2024-07-29 15:22:54 +0530

    car animation added

--- src/app/component/pages/home/home.component.ts
+++ src/app/component/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  carLImages: string[] = [
+    'assets/carL/carL.png',
+    'assets/carL/carL2.png',
+    'assets/carL/carL3.png',
+    'assets/carL/carL4.png'
+  ];
+  
+  carRImages: string[] = [
+    'assets/carR/carR.png',
+    'assets/carR/carR2.png',
+    'assets/carR/carR3.png',
+    'assets/carR/carR4.png'
+  ];
+  
+  carLImage: string = this.carLImages[0];
+  carRImage: string = this.carRImages[0];
+  
+  private carLIndex: number = 0;
+  private carRIndex: number = 0;
+
+  ngOnInit(): void {
+    setInterval(() => this.updateCarLImage(), 10000);
+    setInterval(() => this.updateCarRImage(), 7500);
+  }
+
+  private updateCarLImage(): void {
+    this.carLIndex = (this.carLIndex + 1) % this.carLImages.length;
+    this.carLImage = this.carLImages[this.carLIndex];
+  }
+
+  private updateCarRImage(): void {
+    this.carRIndex = (this.carRIndex + 1) % this.carRImages.length;
+    this.carRImage = this.carRImages[this.carRIndex];
+  }
 
 }
